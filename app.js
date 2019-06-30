6957e3bb19ceed235b525effb428da07940eb0df
Madhur Kapoor <madhur.kapoor@open.ac.uk>
2019-06-30 15:57:59 +0530

    added express session

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,12 +30,21 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+
+}));
 
 function auth(req, res, next) {
   console.log(req.headers);
 
-  if(!req.signedCookies.user) {
+  if(!req.session.user) {
 
     var authHeader = req.headers.authorization;
 
@@ -53,7 +64,7 @@ function auth(req, res, next) {
   var pass = auth[1];
 
   if(user === 'admin' && pass === 'password') {
-    res.cookie('user', 'admin',{signed:true});
+    req.session.user = 'admin';
     next();
   }
   else {
