873731d0671a87569892122c14853bcef49a811c
Madhur Kapoor <madhur.kapoor@open.ac.uk>
2019-06-30 15:22:03 +0530

    Added Cookies during authentication

--- app.js
+++ app.js
@@ -28,12 +28,14 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
   console.log(req.headers);
 
-  var authHeader = req.headers.authorization;
+  if(!req.signedCookies.user) {
+
+    var authHeader = req.headers.authorization;
 
   if(!authHeader) {
     var err = new Error("You are not authenticaterd");
@@ -51,6 +53,7 @@ function auth(req, res, next) {
   var pass = auth[1];
 
   if(user === 'admin' && pass === 'password') {
+    res.cookie('user', 'admin',{signed:true});
     next();
   }
   else {
@@ -61,6 +64,17 @@ function auth(req, res, next) {
     return;
   }
 
+}
+  else {
+    if (req.signedCookies.user === 'admin') {
+      next();
+  }
+  else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      next(err);
+  }
+}
 }
 
 app.use(auth);
